75c0873a8e1754a8cfd1452ced85bb06fc99a437
dongyeop915 <dongyeop915@gmail.com>
2020-08-30 18:51:36 +0900

    init

---
+++ ScreenSizeModifier/MainWindow.xaml.cs
@@ -0,0 +1,40 @@
+﻿using ScreenSizeModifier.NewFolder1;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace ScreenSizeModifier
+{
+    /// <summary>
+    /// MainWindow.xaml에 대한 상호 작용 논리
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+
+        ScreenChanger changer;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            changer = new ScreenChanger();
+
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            changer.ChangeSize();
+        }
+    }
+}
---
+++ ScreenSizeModifier/NewFolder1/ScreenChanger.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Forms;
+
+namespace ScreenSizeModifier.NewFolder1
+{
+
+
+    public class ScreenChanger
+    {
+        [DllImport("user32.dll")]
+        static extern IntPtr GetActiveWindow();
+
+
+        [DllImport("user32.dll", SetLastError = true)]
+        internal static extern bool MoveWindow(IntPtr hWnd, int X, int Y, int nWidth, int nHeight, bool bRepaint);
+
+        public void ChangeSize()
+        {
+            IntPtr activeWindow = GetActiveWindow();
+
+            MoveWindow(activeWindow, 0, 0, Screen.PrimaryScreen.Bounds.Width + 10, Screen.PrimaryScreen.Bounds.Height / 2, true);
+
+
+        }
+
+
+    }
+}
